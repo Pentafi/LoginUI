d3a17f572735aaf2db033ffcb97aff910b0c6ada
Earl Michael Filosopo <earl.filosopo@usjr.edu.ph>
2022-10-19 16:18:50 +0800

    Initial Commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,130 @@
+
+// ignore_for_file: unnecessary_string_escapes
+
+import 'package:flutter/material.dart';
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              Column(
+                children: [
+                  Image.asset('assets/images/PUTAHE.png',
+              height: 200,
+              width: 200,
+            ),
+                ]
+                ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+
+              const SizedBox(
+                height: 5,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: const Text("Forget Password?"))
+                ],
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.brown,
+                ),
+
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontFamily: 'borsok',
+                      fontSize: 25,
+                      color: Color.fromARGB(190, 227, 179, 128),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              const Divider(
+                height: 30,
+                color: Colors.black,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an Account?",
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                    ),
+                  ),
+                  
+                  TextButton(
+                      onPressed: () {}, child: const Text("Register Account"))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import './login_screen.dart';
+
+void main() => runApp(const LoginUI());
+
+class LoginUI extends StatelessWidget {
+  const LoginUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Login UI",
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: const Color(0xffe3b370),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            " ",
+          ),
+        ),
+        body: const LoginScreen(),
+      ),
+    );
+  }
+}
